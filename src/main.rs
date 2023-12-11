1a2bde1a2f45abdc2c4e1bb5591ad2a93d01fad1
Robert Schaap <robert.schaap@shell.com>
2023-12-11 19:32:18 +0100

    Create some components and system

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,29 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_systems(Update, hello_world)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
         .run();
 }
 
 fn hello_world() {
     println!("hello bevy!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Foo".to_string())));
+    commands.spawn((Person, Name("Bar".to_string())));
+    commands.spawn((Person, Name("Qux".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0)
+    }
 }
