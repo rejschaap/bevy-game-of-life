ce65c08ef48b24c1b06bda82cf828cbbea2778b0
Robert Schaap <robert.schaap@shell.com>
2023-12-11 22:15:41 +0100

    Add default plugins

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_plugins(DefaultPlugins)
         .add_systems(Startup, add_people)
         .add_systems(Update, (hello_world, greet_people))
         .run();
