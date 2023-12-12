4d170cba7af01f6a7d3e9900daf19e1f080188ff
Robert Schaap <robert.schaap@shell.com>
2023-12-12 23:28:51 +0100

    Update cells in FixedUpdate

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,53 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .init_resource::<Game>()
         .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, (update, render))
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
 
 #[derive(Resource, Default)]
 struct Game {
-    width: i8,
-    height: i8,
+    width: usize,
+    height: usize,
+    board: Vec<Vec<Cell>>,
+}
+
+struct Cell {
+    alive: bool,
 }
 
 fn setup(mut game: ResMut<Game>) {
     game.width = 32;
     game.height = 20;
+
+    game.board = (0..game.height)
+        .map(|j| {
+            (0..game.width)
+            .map(|i| {
+                let alive = (i + j) % 2 == 0;
+                Cell { alive }
+            })
+            .collect()
+        })
+        .collect()
+}
+
+fn update(mut game: ResMut<Game>) {
+    let previous = &game.board;
+
+    game.board = (0..game.height)
+    .map(|j| {
+        (0..game.width)
+        .map(|i| {
+            let alive = !previous[j][i].alive;
+            Cell { alive }
+        })
+        .collect()
+    })
+    .collect()
+}
+
+fn render(game: Res<Game>) {
+    println!("Value @ (5, 3) = {}", game.board[5][3].alive)
 }
