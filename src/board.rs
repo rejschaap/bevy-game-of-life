2da469d62261414d95fb12522b1770548ce6b7c7
Robert Schaap <robert.schaap@shell.com>
2023-12-15 13:31:38 +0100

    Add a unit test

--- src/board.rs
+++ src/board.rs
@@ -1,11 +1,19 @@
-pub fn create_board_checkered(width: usize, height: usize) -> Vec<Vec<bool>> {
+pub fn create_board_empty(width: usize, height: usize) -> Vec<Vec<bool>> {
     (0..height)
-        .map(|j| (0..width).map(|i| (i + j) % 2 == 0).collect())
+        .map(|_| (0..width).map(|_| false).collect())
         .collect()
 }
 
-pub fn create_board_empty(width: usize, height: usize) -> Vec<Vec<bool>> {
-    (0..height)
-        .map(|j| (0..width).map(|i| false).collect())
-        .collect()
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_create_board_empty() {
+        let board = create_board_empty(2, 3);
+
+        assert_eq!(board.len(), 3);
+        assert_eq!(board[0].len(), 2);
+        assert_eq!(board[2][1], false);
+    }
 }
